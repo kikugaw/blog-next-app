3bf653dd246cc334f565f0f1c03e8501de6c3d2f
kikugaw <kk.019.nick@gmail.com>
2024-08-04 17:27:42 +0900

    画像も一緒に投稿できるように修正

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Container from "../../components/container";
 import distanceToNow from "../../lib/dateRelative";
 import { getAllPosts } from "../../lib/getPost";
+import Image from "next/image";
 
 export default function NotePage({
   allPosts,
@@ -12,16 +13,27 @@ export default function NotePage({
       {allPosts.length ? (
         allPosts.map((post) => (
           <article key={post.slug} className="mb-10">
-            <Link
-              as={`/posts/${post.slug}`}
-              href="/posts/[slug]"
-              className="text-lg leading-6 font-bold"
-            >
-              {post.title}
-            </Link>
-            <p>{post.excerpt}</p>
-            <div className="text-gray-400">
-              <time>{distanceToNow(new Date(post.date))}</time>
+            <div className="flex">
+              <Image
+                src={post.image}
+                alt="sweet"
+                width={100}
+                height={100}
+                className="rounded-full mt-6 rounded-lg shadow-xl"
+              />
+              <div className="flex-grow p-4">
+                <Link
+                  as={`/posts/${post.slug}`}
+                  href="/posts/[slug]"
+                  className="text-lg leading-6 font-bold"
+                >
+                  {post.title}
+                </Link>
+                <p>{post.excerpt}</p>
+                <div className="text-gray-400">
+                  <time>{distanceToNow(new Date(post.date))}</time>
+                </div>
+              </div>
             </div>
           </article>
         ))
@@ -33,7 +45,7 @@ export default function NotePage({
 }
 
 export async function getStaticProps() {
-  const allPosts = getAllPosts(["slug", "title", "excerpt", "date"]);
+  const allPosts = getAllPosts(["slug", "title", "excerpt", "image", "date"]);
 
   return {
     props: { allPosts },
